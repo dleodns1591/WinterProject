2870c079213ca97bab233ca317dc14f8fa7ae2fc
576rick1412 <576rick1412@gmail.com>
2022-01-24 11:16:55 +0900

    추가

--- Assets/LeeSeongjun/CS/UI_Folder/Long_UI/Long_UI_Main.cs
+++ Assets/LeeSeongjun/CS/UI_Folder/Long_UI/Long_UI_Main.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Long_UI_Main : MonoBehaviour
 {
-    public GameObject Long_UI_non;
+    public GameObject Long_UI_Profile;
     public GameObject Long_UI_Inventory;
     public GameObject Long_UI_Card;
     public GameObject Long_UI_Map;
@@ -13,12 +13,7 @@ public class Long_UI_Main : MonoBehaviour
     public int Long_UI_Level;
     void Start()
     {
-        Long_UI_non.gameObject.SetActive(true);
-
-        Long_UI_Inventory.gameObject.SetActive(false);
-        Long_UI_Card.gameObject.SetActive(false);
-        Long_UI_Map.gameObject.SetActive(false);
-        Long_UI_Monster.gameObject.SetActive(false);
+        Long_UI_Level = 0;
     }
 
     // Update is called once per frame
@@ -27,29 +22,36 @@ public class Long_UI_Main : MonoBehaviour
         //Main_Number oc = GameObject.Find("¹Ù±ù Ç¥Áö ±âº»xÁÂÇ¥ -540 , -1380").GetComponent<Main_Number>();
         switch (Long_UI_Level)
         {
+            case 0:
+                Long_UI_Profile.gameObject.SetActive   ( true);
+                Long_UI_Inventory.gameObject.SetActive (false);
+                Long_UI_Card.gameObject.SetActive      (false);
+                Long_UI_Map.gameObject.SetActive       (false);
+                Long_UI_Monster.gameObject.SetActive   (false);
+                break;
             case 1:
-                Long_UI_non.gameObject.SetActive       (false);
+                Long_UI_Profile.gameObject.SetActive   (false);
                 Long_UI_Inventory.gameObject.SetActive ( true);
                 Long_UI_Card.gameObject.SetActive      (false);
                 Long_UI_Map.gameObject.SetActive       (false);
                 Long_UI_Monster.gameObject.SetActive   (false);
                 break;
             case 2:
-                Long_UI_non.gameObject.SetActive       (false);
+                Long_UI_Profile.gameObject.SetActive   (false);
                 Long_UI_Inventory.gameObject.SetActive (false);
                 Long_UI_Card.gameObject.SetActive      ( true);
                 Long_UI_Map.gameObject.SetActive       (false);
                 Long_UI_Monster.gameObject.SetActive   (false);
                 break;
             case 3:
-                Long_UI_non.gameObject.SetActive       (false);
+                Long_UI_Profile.gameObject.SetActive   (false);
                 Long_UI_Inventory.gameObject.SetActive (false);
                 Long_UI_Card.gameObject.SetActive      (false);
                 Long_UI_Map.gameObject.SetActive       ( true);
                 Long_UI_Monster.gameObject.SetActive   (false);
                 break;
             case 4:
-                Long_UI_non.gameObject.SetActive       (false);
+                Long_UI_Profile.gameObject.SetActive   (false);
                 Long_UI_Inventory.gameObject.SetActive (false);
                 Long_UI_Card.gameObject.SetActive      (false);
                 Long_UI_Map.gameObject.SetActive       (false);
