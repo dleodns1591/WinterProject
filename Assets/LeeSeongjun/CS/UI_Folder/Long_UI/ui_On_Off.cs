2d8cffc6272124670f7eea5d3bb6877c85a69b25
576rick1412 <576rick1412@gmail.com>
2022-02-14 14:21:11 +0900

    이제부터 ui는 "TAB"키로 열 수 있습니다

---
+++ Assets/LeeSeongjun/CS/UI_Folder/Long_UI/ui_On_Off.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ui_On_Off : MonoBehaviour
+{
+    public GameObject rightArrow;
+    public GameObject leftArrow;
+    void Update()
+    {
+        Main_Number oc = GameObject.Find("바깥 표지 기본x좌표 -540 , -1380").GetComponent<Main_Number>();
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (oc.Spot == 0)
+            {
+                Debug.Log("UI열림");
+                rightArrow.SetActive(false); //오른쪽 열림 , 비활성화
+                leftArrow.SetActive(true); // 왼쪽은 활성화
+                oc.countN = 0;
+                oc.Spot = 1;
+                Debug.Log("UI열림2");
+            }
+            else if (oc.Spot == 1)
+            {
+                Debug.Log("UI닫힘");
+                rightArrow.SetActive(true); //UI닫힘 , 활성화
+                leftArrow.SetActive(false); // 왼쪽은 비활성화
+                oc.countN = 0;
+                oc.Spot = 0;
+            }
+        }
+
+    }
+}
